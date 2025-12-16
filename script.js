158aafc1e03674520606a0620936b97db3615912
Zupi <49796750+Zupi-HR@users.noreply.github.com>
2025-12-16 15:21:16 +0100

    feat: implement quiz selection and dynamic header updates

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let storedTheme = localStorage.getItem("darkMode");
 const darkModeToggle = document.querySelector(".theme-toggle");
 const headerSubject = document.getElementById("header-subject");
 const quizMenu = document.getElementById("quiz-menu");
+const quizQuestion = document.getElementById("quiz-question");
 
 function enableDarkMode() {
   document.body.dataset.theme = "dark";
@@ -27,9 +28,34 @@ darkModeToggle.addEventListener("change", () => {
   }
 });
 
-quizMenu.addEventListener("click", (event) => {
-  const selectedButton = event.target.closest("button[data-quiz-type]");
-  if (!selectedButton) return;
-  const selectedSubject = selectedButton.dataset.quizType;
-  console.log(selectedSubject);
+async function getData() {
+  try {
+    const response = await fetch("data.json");
+    if (!response.ok) throw new Error("Could not fetch data");
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function startQuiz(quiz) {
+  headerSubject.dataset.quizType = quiz.title;
+  headerSubject.innerHTML = `
+  <img src="${quiz.icon}" alt="">
+  <span>${quiz.title}</span>
+  `;
+  quizMenu.classList.add("hidden");
+  quizQuestion.classList.remove("hidden");
+}
+
+quizMenu.addEventListener("click", async (event) => {
+  const menuOption = event.target.closest("button[data-quiz-type]");
+  if (!menuOption) return;
+  const quizData = await getData();
+  if (!quizData) return;
+  const quizTitle = menuOption.dataset.quizType;
+  const selectedQuiz = quizData.quizzes.find(
+    (quiz) => quiz.title === quizTitle
+  );
+  startQuiz(selectedQuiz);
 });
